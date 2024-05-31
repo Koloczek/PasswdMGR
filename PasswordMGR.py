413f537f15de1dc9f48cf048c4868b58ea1b4195
Koloczek <kacper.kolaczkowski@protonmail.com>
2024-05-31 12:14:59 +0200

    Changes

--- PasswordMGR.py
+++ PasswordMGR.py
@@ -33,8 +33,9 @@ def get():
         with open("passwords.txt", 'r') as f:
             for k in f:
                 i = k.split(' ')
-                
+                print(f"Read line: {i}")
                 passwords[i[1]] = decrypt_password(i[2].strip())
+                print(f"Decrypted password for {i[1]}: {passwords[i[1]]}")
     except Exception as e:
        print(f"ERROR: {e}")
 
@@ -42,7 +43,7 @@ def get():
         mess = "Your passwords:\n"
         for user in passwords:
             if user == username:
-                mess += f"Password for {username} is {passwords[i]}\n"
+                mess += f"Password for {username} is {passwords[user]}\n"
                 break
         else:
             mess += "No Such Username Exists !!"
@@ -50,12 +51,9 @@ def get():
     else:
         messagebox.showinfo("Passwords", "EMPTY LIST!!")
 
-
 def getlist():
-    # creating a dictionary
     passwords = {}
 
-    # adding a try block, this will catch errors such as an empty file or others
     try:
         with open("passwords.txt", 'r') as f:
             for k in f:
@@ -67,22 +65,19 @@ def getlist():
     if passwords:
         mess = "List of passwords:\n"
         for name, password in passwords.items():
-            # generating a proper message
+            
             mess += f"Password for {name} is {password}\n"
-        # Showing the message
+       
         messagebox.showinfo("Passwords", mess)
     else:
         messagebox.showinfo("Passwords", "Empty List !!")
 
 
 def delete():
-    # accepting input from the user
     username = entryName.get()
 
-    # creating a temporary list to store the data
     temp_passwords = []
 
-    # reading data from the file and excluding the specified username
     try:
         with open("passwords.txt", 'r') as f:
             for k in f:
@@ -90,7 +85,6 @@ def delete():
                 if i[0] != username:
                     temp_passwords.append(f"{i[0]} {i[1]}")
 
-        # writing the modified data back to the file
         with open("passwords.txt", 'w') as f:
             for line in temp_passwords:
                 f.write(line)
@@ -105,38 +99,31 @@ if __name__ == "__main__":
     app = tk.Tk()
     app.geometry("560x270")
     app.title("UMWS Password Manager")
-
-    # Username block
+   
     labelName = tk.Label(app, text="WEBSITE:")
     labelName.grid(row=0, column=0, padx=10, pady=5)
     entrySite = tk.Entry(app)
     entrySite.grid(row=0, column=1, padx=10, pady=5)
 
-    # Username block
     labelName = tk.Label(app, text="USERNAME:")
     labelName.grid(row=1, column=0, padx=10, pady=5)
     entryName = tk.Entry(app)
     entryName.grid(row=1, column=1, padx=10, pady=5)
 
-    # Password block
     labelPassword = tk.Label(app, text="PASSWORD:")
     labelPassword.grid(row=2, column=0, padx=10, pady=5)
     entryPassword = tk.Entry(app)
     entryPassword.grid(row=2, column=1, padx=10, pady=5)
 
-    # Add button
     buttonAdd = tk.Button(app, text="Add", command=add)
     buttonAdd.grid(row=3, column=0, padx=15, pady=8, sticky="we")
 
-    # Get button
     buttonGet = tk.Button(app, text="Get", command=get)
     buttonGet.grid(row=3, column=1, padx=15, pady=8, sticky="we")
 
-    # List Button
     buttonList = tk.Button(app, text="List", command=getlist)
     buttonList.grid(row=4, column=0, padx=15, pady=8, sticky="we")
 
-    # Delete button
     buttonDelete = tk.Button(app, text="Delete", command=delete)
     buttonDelete.grid(row=4, column=1, padx=15, pady=8, sticky="we")
 
